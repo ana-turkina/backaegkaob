d94e73d279f390534a6e88711a729b77f11c17c1
Anastassia <Anastassia.ana.turkina@gmail.com>
2023-12-14 12:17:43 +0200

    Implement user profile update functionality

--- src/main/java/ee/valiitit/backaegkaob/domain/profile/ProfileService.java
+++ src/main/java/ee/valiitit/backaegkaob/domain/profile/ProfileService.java
@@ -1,15 +1,19 @@
-package ee.valiitit.backaegkaob.domain.userprofile;
+package ee.valiitit.backaegkaob.domain.profile;
 
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserProfileService {
+public class ProfileService {
     @Resource
     private ProfileRepository profileRepository;
 
     public Profile getProfileBy(Integer userId) {
         return profileRepository.getProfileBy(userId);
 
+    }
+
+    public void saveProfile(Profile profile) {
+        profileRepository.save(profile);
     }
 }
